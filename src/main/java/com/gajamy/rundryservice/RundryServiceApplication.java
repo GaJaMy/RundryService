3c68956488f92418298404538318236b61d9b59d
HyungDong <mansa0805@gmail.com>
2022-10-18 16:29:41 +0900

    Initial commit

---
+++ src/main/java/com/gajamy/rundryservice/RundryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gajamy.rundryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RundryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RundryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gajamy/rundryservice/RundryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gajamy.rundryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RundryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
